dc528cd67f310632c421f57d89fd91ffd98d090c
Daniel98Kamau <128593418+Daniel98Kamau@users.noreply.github.com>
2023-06-10 17:27:40 +0300

    API progress

---
+++ blogapi/posts/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .permissions import IsAuthorOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
